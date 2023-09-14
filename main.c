e3b37c379ddf8f1e8186b847ee68b683b34f6482
Woraphon Sadfak <118672468+Markkerg@users.noreply.github.com>
2023-09-14 21:52:43 +0700

    start new once...

--- main.c
+++ main.c
@@ -1,58 +1,26 @@
-#include <SDL.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<unistd.h>
+#include"character.c"
+#include"act1.c"
 
-int WinMain(int argc, char* argv[]) {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return 1;
+void slowPrint(const char *text);
+int check,process=0;
+
+int main(){
+    printf("Welcome to the game \"Ghost, haunted spirit\"\nPress 1 : Start game.\nPress 0 : Exit.\n");
+    scanf("%d", &check);
+    if( check == 0){
+        return 0;
     }
-
-    // Create a window
-    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        return 2;
+    while(1){
+        mornigatschool();
     }
+}
 
-    // Create a renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL) {
-        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
-        return 3;
+void slowPrint(const char *text) {
+    for (int i = 0; text[i] != '\0'; i++) {
+        printf("%c", text[i]);
+        fflush(stdout); // อัพเดทข้อมูลที่ถูกบันทึกไว้ใน stdout
+        usleep(7000); // หน่วงเวลา 50 ไมโครวินาที (0.05 วินาที)
     }
-
-    // Event loop
-    SDL_Event event;
-    int quit = 0;
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = 1;
-            }
-        }
-
-        // Clear the renderer with a white background
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderClear(renderer);
-
-        // Draw a black rectangle in the center
-        SDL_Rect rect;
-        rect.x = 350; // X-coordinate
-        rect.y = 250; // Y-coordinate
-        rect.w = 100; // Width
-        rect.h = 100; // Height
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set color to black
-        SDL_RenderFillRect(renderer, &rect);
-
-        // Render everything
-        SDL_RenderPresent(renderer);
-    }
-
-    // Clean up and exit
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
 }
